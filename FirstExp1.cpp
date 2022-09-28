370055eb7c0c699a51bc17fe13cb9dfcfcd65245
Anton Korenik <66888032+antonkorenik@users.noreply.github.com>
2022-09-28 18:14:06 +0300

    1
    1

---
+++ FirstExp1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int a = 100;
+int n = 2;
+int main()
+{
+	setlocale(0, "");
+	cout << "n = " << n << endl;
+	cout << "a = " << a << endl << "a % n = " << a % n << endl;
+
+	a = 256;
+	int k = n % 5;
+	cout << "a = " << a << endl << "a >> k = " << (a >> k) << endl;
+
+	a = 2;
+	k = n % 5;
+	cout << "a = " << a << endl << "a << k = " << (a << k) << endl;
+	cout << boolalpha;
+	cout << "((6 % 3 == 1) == (0 == 1)) = " << ((6 % 3 == 1) == (0 == 1)) << endl;
+	cout << "((2 != 2*3*4) && (2 != 24)) = " << ((2 != 2 * 3 * 4) && (2 != 24)) << endl;
+	cout << "((5 / 2 >= 5) || (2 >= 5)) = " << ((5 / 2 >= 5) || (2 >= 5)) << endl;
+	cout << "((8 % 3 == 1) == (2 == 1)) = " << ((8 % 3 == 1) == (2 == 1)) << endl;
+	cout << "((2 != 1*1*2) == (!2 != 2)) = " << ((2 != 1 * 1 * 2) == (!2 != 2)) << endl;
+
+	return 0;
+}
+		
