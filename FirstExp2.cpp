6bc3502752efc172fce195c25c74c5b80f7dd33f
Anton Korenik <66888032+antonkorenik@users.noreply.github.com>
2022-09-29 16:07:39 +0300

    3
    3

---
+++ FirstExp2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+float n;
+int main()
+{
+	while (true)
+	{
+		setlocale(0, "");
+		cout << "Введите n: ";
+		cin >> n;
+		if (n > 0) break;
+		cout << endl << "Введено отрицательное n!!! "
+			<< endl << "Попробуйте еще раз!!!" << endl;
+	}
+	cout << "Результат: " << (pow(2, n));
+	return 0;
+}
