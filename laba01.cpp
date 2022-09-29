22b287398a16ad753b0f009e8280f6dfc8170c18
Anton Korenik <66888032+antonkorenik@users.noreply.github.com>
2022-09-29 19:46:29 +0300

    extra
    extra

---
+++ laba01.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+int a, b;
+using namespace std;
+int main()
+{
+	cout << "Enter a " << endl;
+	cin >> a;
+	cout << "Enter b " << endl;
+	cin >> b;
+	if (a % b == 0)
+		cout << "Result: " << a * b;
+	else cout << "fail";
+	return  0;
+}
+/*#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const float RADIUS = 1.5; //глобальная константа
+const float A = -1;
+float X, Y;
+
+bool BelongsCircle(double x, double y) //прототип функции
+{
+	return x * x + y * y <= pow(RADIUS, 2);
+}
+
+int main()
+{
+	if (X >= A) cout << "Missing" << endl;
+	else cout << "OK";
+	return 0;
+}*/
