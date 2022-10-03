364eca13286df1e296d5e2b1b39310bec7ddc327
Anton Korenik <66888032+antonkorenik@users.noreply.github.com>
2022-10-03 16:10:26 +0300

    5
    5

--- laba01.cpp
+++ laba01.cpp
@@ -1,40 +1,35 @@
 ﻿#include <iostream>
 #include <cmath>
 using namespace std;
-const float a = 3, b = 4;
-float h, x;
 int main()
 {
 	setlocale(0, "");
+	const unsigned DIM = 10;
+	int n, negatives = 0;
+	double a[DIM], negSum = 0, posSum = 0;
 	while (true)
 	{
-		cout << "Введите значение деления шкалы: ";
-		cin >> h;
-		if (h < b - a) break;
-		cout << endl << "Неверное значение!"
+		cout << "Введите n <= " << DIM << endl;
+		cin >> n;
+		if ((n > 0) && (n <= DIM)) break;
+		cout << "Количество элементов указано не верно."
 			<< endl << "Попробуйте еще раз!" << endl;
 	}
-	cout << endl << "Таблица значений функций"
-		<< endl << endl;
-	cout.width(34);
-	cout.fill('*');
-	cout << ' ' << endl
-		<< "* x * value *" << endl;
-	cout.width(34);
-	cout.fill('*');
-	cout << ' ' << endl;
-	cout.precision(5);
-	cout.setf(ios::scientific);
-	x = a;
-	while (x <= b)
+	srand(time(0));
+	cout << "Введите значение массива: " << endl;
+	for (int i = 0; i < n; i++)
 	{
-		cout << "* " << x << " * " << x * sqrt(x) << " *"
-			<< endl;
-		x = x + h;
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+		if (a[i] < 0)
+		{
+			negatives++;
+			negSum = negSum + a[i];
+		}
+		else if (a[i] > 0) posSum = posSum + a[i];
 	}
-	cout.unsetf(ios::scientific);
-	cout.width(34);
-	cout.fill('*');
-	cout << ' ' << endl;
-	return 0;
+	cout << "Количество отрицательных элементов: " << negatives << endl
+		<< "Сумма отрицательных элементов: " << negSum << endl
+		<< "Сумма положительных элементов: " << posSum;
+	return(0);
 }
